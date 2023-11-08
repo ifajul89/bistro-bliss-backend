4a3c23a595cd811faa79a26e632988b6ab9b8ace
Blue Bird <bluebird089@proton.me>
2023-11-08 11:44:54 +0600

    fixed server running issue

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-// Middlewares
 app.use(cors());
-app.use(express.json);
+app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("Server is running");
+    res.send("Server is Running");
 });
 
 app.listen(port, () => {
